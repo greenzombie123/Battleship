83d04bab4f79324866a893f6604b342afa734b1c
Alvin Hart <pageking123@gmail.com>
2023-06-16 15:47:56 +0900

    Add properties of ComputerAIAttack

--- modules/ComputerAIAttack.js
+++ modules/ComputerAIAttack.js
@@ -1,5 +1,12 @@
-export default class ComputerAIAttack{
-    constructor(){
-        
-    }
+export default class ComputerAIAttack {
+  constructor() {
+    this.madeFirstHit = false;
+    this.madeSecondHit = false;
+    this.firstHitCoordinates = [];
+    this.adjacentCoordinates = null;
+    this.currentAdjacentCoordinates = null;
+    this.previousAdjacentCoordinates = [];
+    this.followingCoordinates = null;
+    this.currentFollowingCoordinates = {};
+  }
 }
