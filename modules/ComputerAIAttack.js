11cef9ab3138557d2bba5395c0dfe4abbb62a1e7
Alvin Hart <pageking123@gmail.com>
2023-06-16 22:44:06 +0900

    Change attackRandomTile

--- modules/ComputerAIAttack.js
+++ modules/ComputerAIAttack.js
@@ -10,16 +10,27 @@ export default class ComputerAIAttack {
     this.currentFollowingCoordinates = {};
   }
 
-  computerMakeAttack(){}
+  computerMakeAttack() {}
 
-  attackRandomTile(){
-    const coordinate = [];
-    coordinate[0] = Math.floor(Math.random() * 10);
-    coordinate[1] = Math.floor(Math.random() * 10);
-    return coordinate;
+  attackRandomTile(playerOneBoard) {
+    const coordinates = [];
+    let isMiss = true;
+
+    while (isMiss) {
+      coordinates[0] = Math.floor(Math.random() * 10);
+      coordinates[1] = Math.floor(Math.random() * 10);
+      isMiss = this.hasMiss(playerOneBoard, coordinates)
+    }
+
+    return coordinates;
   }
 
-  setFirstHitCoordinates(coordinates){
-    this.firstHitCoordinates = coordinates
+  hasMiss(playerOneBoard, coordinates) {
+    const [y, x] = coordinates;
+    return playerOneBoard[y][x] === "M";
+  }
+
+  setFirstHitCoordinates(coordinates) {
+    this.firstHitCoordinates = coordinates;
   }
 }
--- modules/tests/ComputerAIAttack.test.js
+++ modules/tests/ComputerAIAttack.test.js
@@ -15,9 +15,10 @@ beforeEach(() => {
 
 describe("attackRandomTile", () => {
   test("Generate a number array of two numbers that are not below 0 and above 9", () => {
-    const coordinates = a.attackRandomTile();
-    const isYGood = coordinates[0] > 0 && coordinates[0] < 10;
-    const isXGood = coordinates[1] > 0 && coordinates[1] < 10;
+    const { playerOneBoard } = g.state;
+    const coordinates = a.attackRandomTile(playerOneBoard);
+    const isYGood = coordinates[0] > -1 && coordinates[0] < 10;
+    const isXGood = coordinates[1] > -1 && coordinates[1] < 10;
     const isValid = isXGood && isYGood;
     expect(isValid).toBe(true);
   });
@@ -30,11 +31,25 @@ describe("setFirstHitCoordinates", () => {
   });
 });
 
-// describe("", ()=>{
-//   test("", ()=>{
-//     expect()
-//   })
-// })
+describe("hasMiss", () => {
+  test("Returns true if the tile on the board has a 'M'", () => {
+    g.state.playerOneBoard[0][0] = "M";
+    const coordinates = [0, 0];
+    const { playerOneBoard } = g.state;
+    const isMiss = a.hasMiss(playerOneBoard, coordinates);
+    expect(isMiss).toBe(true);
+  });
+});
+
+describe("hasMiss", () => {
+  test("Returns false if the tile on the board has no 'M'", () => {
+    g.state.playerOneBoard[0][0] = null;
+    const coordinates = [0, 0];
+    const { playerOneBoard } = g.state;
+    const isMiss = a.hasMiss(playerOneBoard, coordinates);
+    expect(isMiss).toBe(false);
+  });
+});
 
 // describe("", ()=>{
 //   test("", ()=>{
